3f926d0260c4b438a46c2f0c96cba1a5c37dfef1
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-14 11:06:33 -0700

    degugging search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
 
    def index
-     FuelStationService.stations(params[:q])
-
-     redirect_to root_path
+     FuelStationService(params[:q]).stations
    end
 end
--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,9 +1,12 @@
 class FuelStationService
 
-  def self.stations(zip = '80203')
-    binding.pry
-    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&limit=10&api_key=Li5KI5bYBxwK8xOWjWnnhdaoTUXe33jfFCs9cNXR")
-    reponse = JSON.parse(conn.body, symoblize_names: true)
+  def initialize
+    @zip = parasm[:q]
+  end
+  
+  def self.stations
+      conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&limit=10&api_key=Li5KI5bYBxwK8xOWjWnnhdaoTUXe33jfFCs9cNXR")
+      reponse = JSON.parse(conn.body, symoblize_names: true)
   end
 
 end
