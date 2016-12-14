37e1fe55560662a35938de9838da580024cf480c
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-14 10:03:16 -0700

    parsing station json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-      conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location='80203'&limit=10&api_key=Li5KI5bYBxwK8xOWjWnnhdaoTUXe33jfFCs9cNXR")
-      reponse = JSON.parse(conn.body, symoblize_names: true)
+      @location = Station.location
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,13 @@
 class Station
 
-  def
-    
+  def self.stations
+    stations = FuelStationService.stations["fuel_stations"]
+    stations.each do |station|
+      name  = station["station_name"]
+      address = station["street_address"]
+      fuel_type = station["fuel_type_code"]
+      distance = staiton["access_days_time"]
+    end
   end
 
 
